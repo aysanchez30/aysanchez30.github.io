e74720a5df98368919e89ebb45c932e0718728af
Ava Sanchez <ava@Avas-MacBook-Air-2.local>
2023-09-25 01:24:41 -0400

    assignment 7

---
+++ assignments/assignment7/script.js
@@ -0,0 +1,1 @@
+// i am going to tutoring for this.
--- class/sept14/script.js
+++ class/sept14/script.js
@@ -1,38 +1,20 @@
-// const add = (a, b) => { return a+b;}
-
-// console.log(add(5, 6));
-
-// const square = a => a * a;
-
-// console.log(square(5));
-
-// const hello =() => console.log("Hello Me");
-
-// hello();
-
-// const helloSpecific = userName => console.log("Hello " + userName + "!");
-
-// helloSpecific("Portia");
-
-// const helloFullName = (firstName, lastName) => {
-//     console.log("Hello " + firstName + " " + lastName);
-//     console.log("You are awesome!");
-
-// };
-
-// helloFullName("portia" , "plante");
-
 const begin = () => {
-    document.getElementById("square").classList.add("anim-square");
-}
+    //document.getElementById("square").classList.add("anim-square");
 
-const displayName = () => {
-    const firstName = document.getElementbyId("txt-first-name").value;
-    console.log("Hello " + firstName + "!");
+    const square = document.getElementById("square");
+    const button = document.getElementById("button-move");
+    //is it currently animating
+
+    if(square.classList.contains("anim-square")) {
+        square.classList.remove("anim-sqaure");
+
+    } else {
+        square.classList.add("anim-sqaure");
+        button.innerHTML = "Stop";
+    }
 }
 window.onload = () => {
     //get button, tie function to clickage
     document.getElementById("button-click").onclick = begin;
-    document.getElementById("button-show-name").onclick = displayName;
 }
 
