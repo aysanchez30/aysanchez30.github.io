d84d189c544dd6ceeb1be1412ee9fe1e634444f7
Ava Sanchez <ava@Avas-MacBook-Air-2.local>
2023-10-31 02:39:31 -0400

    formatting

--- project/part5/script.js
+++ project/part5/script.js
@@ -1,50 +1,35 @@
-const getTeam = async () => {
-    const url = "https://aysanchez30.github.io/project/part5/about.json";
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    // URL of the JSON file
+    const jsonURL = "https://aysanchez30.github.io/project/part5/about.json"; // Replace with your JSON file URL
 
-const showTeam = async () => {
-    const team = await getTeam();
-    const teamList = document.getElementById("team-members");
-    
-    team.forEach(member => {
-        teamList.appendChild(getTeamInfo(member));
-    });
-};
+    // HTML elements where JSON data will be displayed
+    const missionContentElement = document.getElementById("mission-content");
+    const teamMembersElement = document.getElementById("team-members");
 
-const getTeamInfo = (member) => {
-    const section = document.createElement("section");
+    // Fetch the JSON data from the URL
+    fetch(jsonURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Display the Mission Statement content
+            missionContentElement.textContent = data["Mission Statement"]["content"];
 
-    const container1 = document.createElement("div");
-    const container2 = document.createElement("div");
-    container2.className = "container2";
-
-    const h2 = document.createElement("h2");
-    h2.innerHTML = member.name;
-
-    const description = document.createElement("p");
-    description.innerHTML = member.description;
-
-    const img = document.createElement("img");
-    img.src = member.img;
-
-    section.appendChild(h2);
-    section.appendChild(container1);
-    section.appendChild(container2);
-
-    container1.appendChild(img);
-
-    container2.appendChild(h2);
-    container2.appendChild(description);
-
-    return section;
-};
-
-window.onload = () => {
-    showTeam();
-}
+            // Display the Team members
+            data["Team"].forEach(member => {
+                const memberElement = document.createElement("div");
+                memberElement.innerHTML = `
+                    <h2>${member.name}</h2>
+                    <p>${member.description}</p>
+                    <img src="${member.img}" alt="${member.name}">
+                `;
+                teamMembersElement.appendChild(memberElement);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching or displaying JSON data:", error);
+        });
+});
